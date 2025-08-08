839c53e17ae0b6c48e15ed82ddee00204dfbba18
Darius Max (maxubrq) <hungtp.play@gmail.com>
2025-08-08 06:46:06 +0000

    build: update build

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,9 @@
+// .puppeteerrc.cjs
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
